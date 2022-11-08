d7964b7ea231968009dc2ae181e49b7d04a58eae
Yolian <105956749+Yolianrodry@users.noreply.github.com>
2022-11-08 03:27:43 -0500

    v3

--- src/main/java/gui/Calculadora.java
+++ src/main/java/gui/Calculadora.java
@@ -6,10 +6,10 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Calculadora extends Application  {
-	
-	public static void main(String args[]){ 
-	      ***(args); 
-	} 
+
+    public static void main(String args[]){
+        launch(args);
+   	 }
 	
 	@Override
 	public void start(Stage window) throws Exception {
