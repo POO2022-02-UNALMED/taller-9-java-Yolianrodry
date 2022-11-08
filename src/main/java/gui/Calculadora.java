f53fa8d4917bcaa71b54a153fdd6838ed4a846f7
Yolian <105956749+Yolianrodry@users.noreply.github.com>
2022-11-08 10:06:19 -0500

    cmd

--- src/main/java/gui/Calculadora.java
+++ src/main/java/gui/Calculadora.java
@@ -6,19 +6,20 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Calculadora extends Application  {
-
-    public static void main(String args[]){
-        launch(args);
-   	 }
 	
+	public static void main(String args[]){ 
+	      launch(args);
+	}
+
 	@Override
 	public void start(Stage window) throws Exception {
-        VBox vb = new VBox();
-        window.setTitle("Calculadora");
+		VBox vb = new VBox();
+		window.setTitle("Calculadora");
 
-        vb.getChildren().add(new Calculator());
-        Scene sc = new Scene(vb, 270, 270);
-        window.setScene(sc);
+		vb.getChildren().add(new Calculator());
+		Scene sc = new Scene(vb, 270, 270);
+		window.setScene(sc);
 		window.show();
-    }
+	}
+
 }
